6593053cab741c613595bc66486fe0ca9dd816d1
MaheyR <mahey.ralambo@edu.univ-fcomte.fr>
2025-11-12 14:38:20 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
+
 // On cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
+const main = document.querySelector("main"); // pour cacher le contenu principal
 
-// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+// Ajoute un écouteur d'événements à l'élément 'toggle'
 toggle.addEventListener("click", () => {
-  // Mise à jour des attributs ARIA pour accessibilité
-  nav.ariaHidden = false;
-  toggle.ariaExpanded = true;
+
+  const isOpen = toggle.ariaExpanded === "true";
+  const isClosed = !isOpen;
+  console.log("isOpen :", isOpen, "isClosed :", isClosed);
+
+  // Si
+  if (isClosed) {
+    nav.ariaHidden = "false";
+    toggle.ariaExpanded = "true";
+    main.setAttribute("hidden", ""); // cache le contenu principal
+  } 
+  // fermeture 
+  else {
+    nav.ariaHidden = "true";
+    toggle.ariaExpanded = "false";
+    main.removeAttribute("hidden");
+  }
 });
